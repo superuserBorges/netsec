b0ff84076688d613acad36aab19a1a5467cb040b
superuserBorges <114955552+superuserBorges@users.noreply.github.com>
2024-11-13 16:29:37 +0100

    Add files via upload

---
+++ MITMAttackApp.cc
@@ -0,0 +1,108 @@
+#include "veins/modules/application/traci/MITMAttackApp.h"
+#include <algorithm>
+#include <sstream>
+
+using namespace veins;
+
+Define_Module(MITMAttackApp);
+
+void MITMAttackApp::initialize(int stage)
+{
+    DemoBaseApplLayer::initialize(stage);
+    
+    if (stage == 0) {
+        // Get configuration parameters
+        nodeId = getParentModule()->getIndex(); // Get the vehicle's index as its ID
+        isAttacker = (nodeId == par("attackerNode").intValue()); // Define attacker based on node ID
+        attackTime = par("attackTime").doubleValue();
+        senderNode = par("senderNode").intValue();
+        
+        // Initialize status
+        hasAttacked = false;
+        hasReceivedWarning = false;
+        hasSentMessage = false;
+        
+        
+        if (isAttacker) {
+            findHost()->getDisplayString().setTagArg("i", 1, "red");
+            findHost()->getDisplayString().setTagArg("is", 0, "2");
+                
+            EV_INFO << "MITM Attack Vehicle initialized as Node " << nodeId << " at " << simTime() << endl;
+        }
+        else {
+            // Only sender vehicle creates timer
+            if (nodeId == senderNode) {
+                sendMessageEvt = new cMessage("sendMessage");
+                // Schedule message slightly before attack time
+                scheduleAt(simTime() + (attackTime - 1), sendMessageEvt);
+                
+                // Make sender vehicle visually distinct
+                findHost()->getDisplayString().setTagArg("i", 1, "blue");
+                EV_INFO << "Sender vehicle " << nodeId << " initialized" << endl;
+            }
+        }
+    }
+}
+
+void MITMAttackApp::handleSelfMsg(cMessage* msg)
+{
+    if (msg == sendMessageEvt && !hasSentMessage && nodeId == senderNode) {
+        sendNormalMessage();
+        hasSentMessage = true;
+        EV_INFO << "Vehicle " << nodeId << " sent normal message at " << simTime() << endl;
+    }
+}
+
+void MITMAttackApp::sendNormalMessage()
+{
+    TraCIDemo11pMessage* wsm = new TraCIDemo11pMessage();
+    populateWSM(wsm);
+    wsm->setDemoData("STATUS:normal");
+    sendDown(wsm);
+}
+
+void MITMAttackApp::onWSM(BaseFrame1609_4* frame)
+{
+    TraCIDemo11pMessage* wsm = check_and_cast<TraCIDemo11pMessage*>(frame);
+    std::string message = wsm->getDemoData();
+    
+    if (isAttacker && !hasAttacked) {
+        if (message.find("STATUS:normal") != std::string::npos) {
+            // RSU modifies and forwards the message
+            TraCIDemo11pMessage* modifiedMsg = wsm->dup();
+            modifiedMsg->setDemoData("WARNING:accident");
+            sendDown(modifiedMsg);
+            
+            hasAttacked = true;
+            EV_INFO << "Vehicle " << nodeId << " (Attacker) modified and forwarded message at " << simTime() << endl;
+        }
+    }
+    else if (!isAttacker && !hasReceivedWarning && nodeId != senderNode) {
+        // Other vehicles receive the modified message
+        if (message.find("WARNING:accident") != std::string::npos) {
+            hasReceivedWarning = true;
+            
+            // Visual feedback
+            findHost()->getDisplayString().setTagArg("i", 1, "red");
+            
+            if (traciVehicle) {
+                traciVehicle->setSpeed(0);
+                traciVehicle->setColor(TraCIColor(255, 0, 0, 255));
+            }
+            
+            EV_INFO << "Vehicle " << nodeId << " stopped due to accident warning at " << simTime() << endl;
+        }
+    }
+}
+
+void MITMAttackApp::finish()
+{
+    if (isAttacker) {
+        EV_INFO << "Attacker vehicle " << nodeId << " attack completed at " << simTime() << endl;
+    }
+    else if (hasReceivedWarning) {
+        EV_INFO << "Vehicle " << nodeId << " was affected by the attack" << endl;
+    }
+    
+    DemoBaseApplLayer::finish();
+}
---
+++ MITMAttackApp.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "veins/modules/application/ieee80211p/DemoBaseApplLayer.h"
+#include "veins/modules/application/traci/TraCIDemo11pMessage_m.h"
+
+namespace veins {
+
+class MITMAttackApp : public DemoBaseApplLayer {
+public:
+    MITMAttackApp() : sendMessageEvt(nullptr),
+                      isAttacker(false), 
+                      hasAttacked(false), hasReceivedWarning(false),
+                      hasSentMessage(false), nodeId(-1), senderNode(-1) {}
+    
+    virtual ~MITMAttackApp() {
+        cancelAndDelete(sendMessageEvt);
+    }
+
+protected:
+    virtual void initialize(int stage) override;
+    virtual void handleSelfMsg(cMessage* msg) override;
+    virtual void onWSM(BaseFrame1609_4* wsm) override;
+    virtual void finish() override;
+
+private:
+    void sendNormalMessage();
+
+    cMessage* sendMessageEvt;
+
+    bool isAttacker;
+    bool hasAttacked;
+    bool hasReceivedWarning;
+    bool hasSentMessage;
+    
+    int nodeId;
+    int senderNode;
+    double attackTime;
+};
+
+}
